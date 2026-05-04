77ccfb6c9c2f499b6757a73fb9fd6a7dac0d6922
shayan <shayanhz.dev@gmail.com>
2026-05-04 15:24:08 -0700

    Add alien colors exercises (if-elif-else)

---
+++ Python-Crash-Course/Basics/Chapter_4_if_statement/alien_color_1.py
@@ -0,0 +1,23 @@
+#H I'm shayan and i want practice today if statement (if,else,elif)
+#I create 3 python file for three color alien and i want to using if statement for each file
+#In my practice alien have 3 colors (green,red,yellow)
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("Nice shot, 5 points earned")
+
+#Alien are green or not
+
+if alien_color == 'green':
+    print("Nice shot, 5 points earned")
+else:
+    print("Nice shot, 10 points earned")
+
+#Finally i have 3 if statement for 3 colors
+
+if alien_color == 'green':
+    print("Nice shot, 5 points earned")
+elif alien_color == 'red':
+    print("Nice shot, 15 points earned")
+else:
+    print("Nice shot, 10 points earned")
