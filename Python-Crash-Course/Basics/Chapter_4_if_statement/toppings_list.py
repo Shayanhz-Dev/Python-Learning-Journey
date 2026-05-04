b39a1376cdba213174b8c018c405e55f73dd259e
shayan <shayanhz.dev@gmail.com>
2026-05-04 16:32:10 -0700

    Add Pizza Toppings order logic using sequential if statements
    -fixed if statement (replaced elife with if)
    -add checking list for special items practice

--- Python-Crash-Course/Basics/Chapter_4_if_statement/toppings_list.py
+++ Python-Crash-Course/Basics/Chapter_4_if_statement/toppings_list.py
@@ -1,10 +1,19 @@
 #Hi I'm Shayan and today i want to practice if statement 
 #I want to know how works "elife"
-reqeusted_toppings = ['mushrooms', 'extra cheese']
-if 'mushrooms' in reqeusted_toppings:
+requested_toppings = ['extra cheese','green peppers','mushrooms']
+if 'mushrooms' in requested_toppings:
     print("Adding mushrooms.")
-elif 'pepperoni' in reqeusted_toppings:
+if 'pepperoni' in requested_toppings:
     print("Adding pepperoni.")
-elif 'extra cheese' in reqeusted_toppings:
+if 'extra cheese' in requested_toppings:
     print("Adding extra cheese")
+print("\nFinished making your pizza!")
+
+#Modify code becuase i want to practice today checking for special item
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now")
+    else:
+        print(f"Adding {requested_topping}.")
 print("\nFinished making your pizza!")
