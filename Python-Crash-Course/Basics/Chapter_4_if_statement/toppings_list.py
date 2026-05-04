8118baed8d83cb3f2c8f26f7676544198b57092f
shayan <shayanhz.dev@gmail.com>
2026-05-04 14:19:30 -0700

    I fixed file names and variable names

--- Python-Crash-Course/Basics/Chapter_4_if_statement/toppings_list.py
+++ Python-Crash-Course/Basics/Chapter_4_if_statement/toppings_list.py
@@ -1,10 +1,10 @@
 #Hi I'm Shayan and today i want to practice if statement 
 #I want to know how works "elife"
-requsted_toppings = ['mushrooms', 'extra cheese']
-if 'mushrooms' in requsted_toppings:
+reqeusted_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in reqeusted_toppings:
     print("Adding mushrooms.")
-elif 'pepperoni' in requsted_toppings:
+elif 'pepperoni' in reqeusted_toppings:
     print("Adding pepperoni.")
-elif 'extra cheese' in requsted_toppings:
+elif 'extra cheese' in reqeusted_toppings:
     print("Adding extra cheese")
 print("\nFinished making your pizza!")
