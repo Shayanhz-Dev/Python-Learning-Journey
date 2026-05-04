27ffc854075bc1e127e432cc380f401b2544b18a
shayan <shayanhz.dev@gmail.com>
2026-05-04 16:15:13 -0700

    Add optimized age categorization exercise
    Implemented life stages using if-elif-else statements. Removed redundant conditional checks for cleaner and more Pythonic code.

---
+++ Python-Crash-Course/Basics/Chapter_4_if_statement/stages_of_life.py
@@ -0,0 +1,17 @@
+#Hi I'm shayan and today I want to practice if statement (if,elif,else)
+#First I create a variable (age) and assign a value to it
+#Next I create if statement to check (age) in the all stages of life
+age = 23
+print("you are in this stage of life:")
+if age <2:
+    print('baby')
+elif age < 4:
+    print('toddler')
+elif age < 13:
+    print('child/kid')
+elif age < 20:
+    print('teenager')
+elif age < 65:
+    print('adult')
+else:
+    print('elder')
