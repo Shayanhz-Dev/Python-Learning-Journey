5cb8eea4896b1bf4a746a53af350d18d02a190dd
shayan <shayanhz.dev@gmail.com>
2026-05-05 09:07:22 -0700

    Complete exercises on creating, accessing, and modifying dictionaries

---
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/alien.py
@@ -0,0 +1,28 @@
+#Hi I'm shayan and today i want to practice about dictionaries and learn about it
+#First i create a dictionarie with key-value pairs
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+#second I want adding new key-value pairs in my dictionarie
+
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+#Third I want start with empty dicstionary
+alien_0 = {}
+alien_0['color'] = 'red'
+alien_0['points'] = 15
+print(alien_0)
+
+#fourth I want modifying values in a dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+
