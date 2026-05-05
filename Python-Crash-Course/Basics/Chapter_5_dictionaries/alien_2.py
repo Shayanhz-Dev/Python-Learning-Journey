4e7bbf603e84ec0c8e80b5f87f974d83faf6c632
shayan <shayanhz.dev@gmail.com>
2026-05-05 09:24:22 -0700

    Add alien movement tracking with dictionaries

---
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/alien_2.py
@@ -0,0 +1,32 @@
+# Hi my name is shayan and I want practice today about tracking alien movement on screen
+# First I make a dictionary
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+
+# Move the alien to the right
+# Determine how far to move the alien based on its current speed.
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+# The new position is the old posistion plus the incerment
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+
+# Now i want change alien speed
+
+alien_0['speed'] = 'fast'
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position with fast speed: {alien_0['x_position']}")
