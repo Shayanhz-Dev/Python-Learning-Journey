e4e75733758229a62faac1cceac7fed6c30aa99b
shayan <shayanhz.dev@gmail.com>
2026-05-05 09:24:47 -0700

    Add example of removing key-value pairs from dictionary

--- Python-Crash-Course/Basics/Chapter_5_dictionaries/alien.py
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/alien.py
@@ -26,3 +26,8 @@ print(f"The alien is {alien_0['color']}.")
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}.")
 
+#Now I want to remove key-value pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+del alien_0['points']
+print(alien_0)
