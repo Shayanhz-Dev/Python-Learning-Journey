33b0d1689c931e83718d9ad568420a9ab07009cd
shayan <shayanhz.dev@gmail.com>
2026-05-06 13:28:04 -0700

    fix comment and names

--- Python-Crash-Course/Basics/Chapter_5_dictionaries/favorite_numbers.py
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/favorite_numbers.py
@@ -1,9 +1,14 @@
-# Hi I'm shayan and today I want to practice dictionary 
-# First , I create a dictionary with key-value
-# I create for-loop for my dictionary 
-# I use .item() method for return key-value to me
-# I print name,numbers key-value
+# Practicing Python dictionaries
+# 1. Create a dictionary with key-value pairs
+# 2. Loop through it using .items()
+# 3. Print each name and its favorite number
 
-favorite_numbers = {'mehdi': 6, 'mozhgan':5, 'sarina':9, 'shayan': 7}
-for name, numbers in favorite_numbers.items():
-    print(f"{name.title()}'s favorite number is {numbers}")
+
+favorite_numbers = {
+    'mehdi': 6, 
+    'mozhgan': 5,
+    'sarina': 9,
+    'shayan': 7,
+                    }
+for name, number in favorite_numbers.items():
+    print(f"{name.title()}'s favorite number is {number}")
