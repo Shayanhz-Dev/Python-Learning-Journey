335f9996c2a2bda69dc67d247edfb805f32dde07
shayan <shayanhz.dev@gmail.com>
2026-05-06 12:21:21 -0700

    feat: create and display person's data using a dictionary

---
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/person.py
@@ -0,0 +1,11 @@
+# Hi I'm shayan and today I want to practice dictionary  for personal information
+# I want to assign personal information in dictionary
+# First of all I create a dictionary with key-value
+# I print each key-value and I print with a message
+
+person = {'first_name': 'kia', 'last_name': 'rad', 'city': 'tehran', 'age': 25}
+print(person['first_name'])
+print(person['last_name'])
+print(person['city'])
+print(person['age'])
+print(f"His name is {person['first_name'].title()} {person['last_name'].title()} from {person['city'].upper()} and he is {person['age']} years old.")
