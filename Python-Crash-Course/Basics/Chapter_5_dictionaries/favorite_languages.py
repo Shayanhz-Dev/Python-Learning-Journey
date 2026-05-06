99a15818b26484894dc26e9c042a4763a0465503
shayan <shayanhz.dev@gmail.com>
2026-05-06 13:36:26 -0700

    Refactor favorite languages to use for-loop and .items()

--- Python-Crash-Course/Basics/Chapter_5_dictionaries/favorite_languages.py
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/favorite_languages.py
@@ -6,3 +6,10 @@ favorite_languages = {
 }
 language = favorite_languages['sarah'].title()
 print(f"sarah's favorite language is {language}.")
+
+# code updated:
+# 1. for-loop for dictionary
+# 2. return key-value with items() method
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}")
