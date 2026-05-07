95164ba26f16e1a2c83c7320cbc52f5a9b354d21
shayan <shayanhz.dev@gmail.com>
2026-05-07 04:01:58 -0700

    Practice .keys() and .values() methods in dictionary iteration

--- Python-Crash-Course/Basics/Chapter_5_dictionaries/favorite_languages.py
+++ Python-Crash-Course/Basics/Chapter_5_dictionaries/favorite_languages.py
@@ -13,3 +13,10 @@ print(f"sarah's favorite language is {language}.")
 
 for name, language in favorite_languages.items():
     print(f"{name.title()}'s favorite language is {language.title()}")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+for language in favorite_languages.values():
+    print(language.title())
+
