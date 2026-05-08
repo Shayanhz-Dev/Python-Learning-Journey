f1d88b2de9622e367ad2ffe82d4615a9211ee373
shayan <shayanhz.dev@gmail.com>
2026-05-08 08:49:25 -0700

    feat: add basic user input and print functionality

---
+++ Python-Crash-Course/Basics/Chapter_6_user_input_and_while_loops/parrot.py
@@ -0,0 +1,5 @@
+# Hi I'm shayan and today I want to start chapter_6 - user input and whilte loops
+# 1. input practice 
+
+message = input("Tell me something, and I will repeat it back to you:")
+print(message)
