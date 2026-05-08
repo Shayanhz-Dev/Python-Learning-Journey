9759eda5f0cba77333608dbc2a47d23484a1a32c
shayan <shayanhz.dev@gmail.com>
2026-05-08 09:12:32 -0700

    feat: use multi-line prompt for user input

---
+++ Python-Crash-Course/Basics/Chapter_6_user_input_and_while_loops/greeter.py
@@ -0,0 +1,12 @@
+# Hi I'm shayan and today I want to practice about input function works
+
+name = input("Please enter your name:")
+print(f"\nHello, {name}")
+
+# Modify code
+# 1. adds the new string into the end.
+
+prompt = "If you share your name, we can personalize the messages you see"
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print(f"\nHello, {name}")
