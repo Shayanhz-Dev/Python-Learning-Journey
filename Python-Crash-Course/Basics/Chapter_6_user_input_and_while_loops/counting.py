0ab07bfc41b56994ce2dc4db145f21c12a241499
shayan <shayanhz.dev@gmail.com>
2026-05-08 10:39:18 -0700

    feat: add basic while-loop counting from 1 to 5

---
+++ Python-Crash-Course/Basics/Chapter_6_user_input_and_while_loops/counting.py
@@ -0,0 +1,8 @@
+# Hi I'm shayan and today I want to practice while-loop
+# 1. Create a variable with value
+# 2. Create a while-loop with Boolean expression
+# 3. Add 1 to the number each time the loop repeats
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number +=1
