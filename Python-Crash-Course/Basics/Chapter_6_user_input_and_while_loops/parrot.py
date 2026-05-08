bbf1763b87c735095e5adf73391e9ff4baccf2ad
shayan <shayanhz.dev@gmail.com>
2026-05-08 10:57:30 -0700

    feat: implement while loop with exit condition using user input

--- Python-Crash-Course/Basics/Chapter_6_user_input_and_while_loops/parrot.py
+++ Python-Crash-Course/Basics/Chapter_6_user_input_and_while_loops/parrot.py
@@ -1,5 +1,19 @@
-# Hi I'm shayan and today I want to start chapter_6 - user input and whilte loops
+# Hi I'm shayan and today I want to start chapter_6 - user input and while loops
 # 1. input practice 
 
 message = input("Tell me something, and I will repeat it back to you:")
 print(message)
+
+# Modified code:
+# 2. Create variable for message
+# 3. Create new variable for checking with while-loop
+# 4. Create while-loop with != 'quit'
+# 5. Add user input to first variable
+prompt = "\nTell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the program. "
+
+message = "\t"
+while message != 'quit':
+    message = input(prompt)
+    if message != "quit":
+         print(message)  
