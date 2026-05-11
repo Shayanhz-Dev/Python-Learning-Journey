785e34889be813ac624f7d105cb92ac2d5310e6a
shayan <shayanhz.dev@gmail.com>
2026-05-11 11:27:43 -0700

    docs: add multiple function calls example to describe_pet

--- Python-Crash-Course/Basics/Chapter_7_functions/pets.py
+++ Python-Crash-Course/Basics/Chapter_7_functions/pets.py
@@ -2,9 +2,14 @@
 # 1. Create function with 'TWO' parameters 
 # 2. Print messages
 # 3. Call function with 2 arguments
+# modified code :
+# Multiple Function Calls
 
 def describe_pet(animal_type, pet_name):
     """Display information about a pet."""
+
     print(f"\nI have a {animal_type.upper()}")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
 describe_pet("cat", "panny")
+describe_pet("dog", "raxar")
