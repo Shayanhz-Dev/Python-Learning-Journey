4837219312b6506a0451d8b426a9855d0cc13796
shayan <shayanhz.dev@gmail.com>
2026-05-11 11:17:34 -0700

    feat: create favorite_book function to practice parameters and arguments

---
+++ Python-Crash-Course/Basics/Chapter_7_functions/favorite_books.py
@@ -0,0 +1,9 @@
+# Hi I'm shayan and today I want to practice Functions
+# 1. Create a function with parameters
+# 2. Print a message with parameters value
+# 3. Call function and pass "Python-Crash-Course" argument
+
+def favorite_book(book):
+    """Display Favorite book"""
+    print(f"One of my favorite books is {book.title()}")
+favorite_book("Python-Crash-Course")
