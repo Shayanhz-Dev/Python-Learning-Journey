a11c39fcb379b7bf75382de6478332268d781c7a
shayan <shayanhz.dev@gmail.com>
2026-05-11 11:24:00 -0700

    feat: implement describe_pet function with multiple parameters

---
+++ Python-Crash-Course/Basics/Chapter_7_functions/pets.py
@@ -0,0 +1,10 @@
+# Hi I'm shayan and today I want to practice about functions
+# 1. Create function with 'TWO' parameters 
+# 2. Print messages
+# 3. Call function with 2 arguments
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type.upper()}")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet("cat", "panny")
