c3c9ec8818c40687c226b5f9ad2404e4d54ae0a2
shayan <shayanhz.dev@gmail.com>
2026-05-12 14:39:04 -0700

    feat: make middle_name argument optional in get_formatted_name function

--- Python-Crash-Course/Basics/Chapter_7_functions/formatted_name _2.py
+++ Python-Crash-Course/Basics/Chapter_7_functions/formatted_name _2.py
@@ -4,11 +4,19 @@
 # 2. Create a variable for formatted name
 # 3. Return variable 
 # 4. Create a variable and assign call+arguments
+# 5. Modified code:
+# 6. making an argument optional
 
-def get_formatted_name(first_name, last_name):
-    """return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = get_formatted_name('john', 'hooker', 'lee')
 print(musician)
