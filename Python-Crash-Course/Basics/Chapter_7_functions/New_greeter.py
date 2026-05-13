a771193faa1d4fcf9a42528ed686fd7f33ca5f2d
shayan <shayanhz.dev@gmail.com>
2026-05-13 10:13:20 -0700

    feat: implement interactive name formatter with quit functionality and refactored title casing

--- Python-Crash-Course/Basics/Chapter_7_functions/New_greeter.py
+++ Python-Crash-Course/Basics/Chapter_7_functions/New_greeter.py
@@ -11,12 +11,22 @@ def get_formatted_name(first_name, last_name):
 # Modified code:
 # This is an infinite loop!
 # 1. Create infinite while-loop
-# 2. getting input values 
+# 2. Getting input values 
 # 3. Call function with variable and input values arguments
+# Second Modified code:
+# 1. Offer a way to quit 
+# 2. Use if-statement for Create exit way
 while True:
     print("\nPlease tell me your name")
+    print("\n(Enter 'Quit' at any time to quit)")
+
     f_name = input("First Name: ").strip().lower()
+    if f_name == 'quit':
+        break
+
     l_name = input("Last Name: ").strip().lower()
+    if l_name == 'quit':
+        break
 
     formatted_name = get_formatted_name(f_name, l_name)
     print(f"\nHello, {formatted_name}")
