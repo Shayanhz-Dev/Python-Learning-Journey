2decab2ba6ac8913ab4b79ba00abbbc69df92070
shayan <shayanhz.dev@gmail.com>
2026-05-13 09:46:33 -0700

    feat: add optional age parameter to build_person function

--- Python-Crash-Course/Basics/Chapter_7_functions/person.py
+++ Python-Crash-Course/Basics/Chapter_7_functions/person.py
@@ -4,11 +4,16 @@
 # 2. Create a dictionary with key-parameters- 
 # 3. Return 
 # 4. Create a variable to Call function with arguments (there is argument is values of the dictionary keys)
+# Modified code:
+# Add age parameter with empty values
+# use If-statement for if parameters have argument-values add age key in the dictionary
 
-
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=None):
     """Return a dictionary of information about a person."""
     person = {'first': first_name, 'last': last_name}
+    if age is not None:
+        person['age'] = age
     return person
-musician = build_person('jimi', 'hendrix')
+
+musician = build_person('jimi', 'hendrix', age=23)
 print(musician)
