786a528c30bf1e83be663623a6749872076d54c8
shayan <shayanhz.dev@gmail.com>
2026-05-14 12:11:05 -0700

    feat: implement build_profile function to demonstrate arbitrary keyword arguments (**kwargs)

---
+++ Python-Crash-Course/Basics/Chapter_7_functions/user_profile.py
@@ -0,0 +1,16 @@
+# Hi I'm shayan and today I want to practice function
+# 1. Create a function with positional parameters and arbitrary keyword arguments
+# 2. Add positional arguments to the dictionary
+# 3. Return the generated dictionary containing user information
+# 4. Call the function and assign the result to a variable
+
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='phtsics')
+print(user_profile)
