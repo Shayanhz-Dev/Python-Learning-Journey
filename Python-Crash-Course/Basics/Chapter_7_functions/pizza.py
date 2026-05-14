8fd409beda2633a3bbc3d144c38fb4cd2118b64b
shayan <shayanhz.dev@gmail.com>
2026-05-14 11:48:59 -0700

    feat: add size parameter to make_pizza combining positional and arbitrary arguments

--- Python-Crash-Course/Basics/Chapter_7_functions/pizza.py
+++ Python-Crash-Course/Basics/Chapter_7_functions/pizza.py
@@ -1,12 +1,12 @@
 # Hi I'm shyana and today I want to practice functions
-# 1. Create a function with arbitrary number of arguments-parameter
-# 2. Function : use for-loop for print each arguments 
-# 3. Positional Call funtion
-def make_pizza(*toppings):
+# 1. Create a function with an arbitrary number of arguments
+# 2. Function: use a for-loop to print each argument 
+# 3. Positional function calls
+def make_pizza(size, *toppings):
     """Summarize the pizza we are about to make."""
-    print("\nmaking a pizza with the following toppings:")
+    print(f"\nMaking a {size}-inch pizza with the following toppings.")
     for topping in toppings:
         print(f"- {topping}")
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
