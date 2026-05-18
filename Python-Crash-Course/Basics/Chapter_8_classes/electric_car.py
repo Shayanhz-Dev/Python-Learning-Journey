583640dd889187390e91a8970814683d604c2b7c
shayan <shayanhz.dev@gmail.com>
2026-05-18 14:50:17 -0700

    feat: implement ElectricCar class utilizing Car inheritance and Battery composition

--- Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
@@ -51,6 +51,14 @@ class Battery:
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kwh battery size.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
 
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
@@ -62,3 +70,4 @@ class ElectricCar(Car):
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
