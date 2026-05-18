5639363d8aeff757d45426d8bd4701046d958409
shayan <shayanhz.dev@gmail.com>
2026-05-18 11:07:46 -0700

    feat: add increment_odometer method to Car class to safely add mileage

--- Python-Crash-Course/Basics/Chapter_8_classes/car.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/car.py
@@ -33,7 +33,19 @@ class Car:
         else:
             print("You can't roll back an odometer!")
     
-my_new_car = Car("audi", 'a4', 2024)
-print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        if miles >= 0:
+            self.odometer_reading += miles
+        else:
+            print("You can't roll back an odometer!")    
+    
+    
+my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
