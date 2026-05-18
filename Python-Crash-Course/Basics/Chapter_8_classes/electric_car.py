b5345be6c2a14f60a039a7c13f66ce2d322792bf
shayan <shayanhz.dev@gmail.com>
2026-05-18 14:31:11 -0700

    feat: add battery attribute and describe_battery method to ElectricCar subclass

--- Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
@@ -45,6 +45,12 @@ class ElectricCar(Car):
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 40
+    
+    def describe_battery(self):
+        """Print a statement describing the battrey size."""
+        print(f"This car has a {self.battery_size}-kwh battery size.")
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()
