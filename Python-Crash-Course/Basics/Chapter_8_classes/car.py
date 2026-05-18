7e35160bcdd2accacf12c58802b0036cf0e64020
shayan <shayanhz.dev@gmail.com>
2026-05-18 10:44:00 -0700

    feat: create Car class with initialization and formatted descriptive name method

---
+++ Python-Crash-Course/Basics/Chapter_8_classes/car.py
@@ -0,0 +1,21 @@
+# Hi I'm shayan and today I want to practice about classes
+# 1. Create a Class
+# 2. Create a method for construction class attribute 
+# 3. Create a method for make a formatted name
+
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+    
+    def get_descriptive_name(self):
+        """return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+my_new_car = Car("audi", 'a4', 2024)
+print(my_new_car.get_descriptive_name())
