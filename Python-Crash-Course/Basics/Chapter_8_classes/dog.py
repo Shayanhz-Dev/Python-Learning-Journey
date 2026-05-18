188cd353f2d998de83f5f004bf73fcc823412055
shayan <shayanhz.dev@gmail.com>
2026-05-18 10:29:11 -0700

    feat: instantiate multiple objects from Dog class to demonstrate independent state and behavior

--- Python-Crash-Course/Basics/Chapter_8_classes/dog.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/dog.py
@@ -20,8 +20,14 @@ class Dog:
         print(f"{self.name} rolled over!")
 
 my_dog = Dog('willie', 6)
+your_dog = Dog("lucy", 3)
 
 print(f"MY dog's name is {my_dog.name}.")
 print(f"MY dog is {my_dog.age} years old.")
 my_dog.sit()
 my_dog.roll_over()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
+your_dog.roll_over()
