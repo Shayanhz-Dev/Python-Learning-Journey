fce16f2b9d4fd88848f5498c033a360409941bc9
shayan <shayanhz.dev@gmail.com>
2026-05-18 14:44:04 -0700

    feat: extract battery logic into a separate Battery class using composition

--- Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
@@ -40,22 +40,25 @@ class Car:
         else:
             print("You can't roll back an odometer!")    
     
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=40):
+        """initialize the battery's attributes."""
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kwh battery size.")
+
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.battery_size = 40
-    
-    def describe_battery(self):
-        """Print a statement describing the battrey size."""
-        print(f"This car has a {self.battery_size}-kwh battery size.")
-
-    def fill_gas_tank(self):
-        """Electric cars dont have gas tanks."""
-        print("this car doesn't have a gas tank!")
+        self.battery = Battery()       
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
-my_leaf.fill_gas_tank()
+my_leaf.battery.describe_battery()
