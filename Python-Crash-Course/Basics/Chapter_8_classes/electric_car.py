fc3f3247b10dcd7af56856e709106a604d81acf2
shayan <shayanhz.dev@gmail.com>
2026-05-18 14:35:27 -0700

    feat: add fill_gas_tank method to ElectricCar to handle incompatible behaviors

--- Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/electric_car.py
@@ -51,6 +51,11 @@ class ElectricCar(Car):
         """Print a statement describing the battrey size."""
         print(f"This car has a {self.battery_size}-kwh battery size.")
 
+    def fill_gas_tank(self):
+        """Electric cars dont have gas tanks."""
+        print("this car doesn't have a gas tank!")
+
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
+my_leaf.fill_gas_tank()
