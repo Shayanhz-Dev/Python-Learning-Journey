2785b503a8358a0d36ae435c257b76c37b43d9cc
shayan <shayanhz.dev@gmail.com>
2026-05-20 14:00:09 -0700

    feat: add login attempts tracking and reset functionality to User class

--- Python-Crash-Course/Basics/Chapter_8_classes/users.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/users.py
@@ -4,29 +4,40 @@
 
 class User:
 
-    def __init__(self, f_name, l_name, age, location):
-
+    def __init__(self, f_name, l_name, age, location, login_attempts=0):
         """Represent a user's information"""
 
         self.f_name = f_name
         self.l_name = l_name
         self.age = age
         self.location = location
+        self.login_attempts = login_attempts
 
     def describe_user(self):
-
-        """Display a summary of the users information"""
+        """Display a summary of the user's information"""
 
         information = f"{self.f_name.title()} {self.l_name.title()} is {self.age} from {self.location}"
         print(information)
 
     def greet_user(self):
-
         """Display a greeting message"""
         
         print(f"Hello {self.f_name.title()} {self.l_name.title()} welcome!")
 
-first_user = User('shayan', 'hasanzade', 23, 'amol')
+    def increment_login_attempts(self):
+        """Counting login attempts"""
 
+        self.login_attempts += 1 
+        print(f"New login attempts {self.login_attempts}")
+
+    def reset_login_attempts(self):
+        """Reset user login attempts"""
+        
+        self.login_attempts = 0
+        print(f"Login attempts reset, now is {self.login_attempts}")
+
+first_user = User('shayan', 'hasanzade', 23, 'amol', 4)
+first_user.increment_login_attempts()
+first_user.reset_login_attempts()
 first_user.describe_user()
 first_user.greet_user()
