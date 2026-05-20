14da0286017a98fd0048b63dedff592475f5f7d1
shayan <shayanhz.dev@gmail.com>
2026-05-20 14:38:12 -0700

    feat: implement IceCreamStand subclass inheriting from Restaurant with flavors list

--- Python-Crash-Course/Basics/Chapter_8_classes/restaurant.py
+++ Python-Crash-Course/Basics/Chapter_8_classes/restaurant.py
@@ -4,6 +4,9 @@
 # 3. Create methods and use attributes 
 # 4. Call attributes 
 # 5. Call methods
+# 6. Create a child class with parent attributes and special attributes
+# 7. Create methods for child class
+# 8. Call methods 
 
 class Restaurant:
     """Represent a restaurant and its status."""
@@ -23,9 +26,32 @@ class Restaurant:
         restaurant_status = f"The restaurant is {self.status}"
         print(restaurant_status)
 
-first_restaurant = Restaurant('parsian', 'persian food', 'open')
+class IceCreamStand(Restaurant):
+    """A Icecreamstand for free space in restaurant"""
 
-print(f"Name attribute : {first_restaurant.restaurant_name.title()}")
-print(f"Cuisine type : {first_restaurant.cuisine_type.upper()}\n")
-first_restaurant.describe_restaurant()
-first_restaurant.open_restaurant()
+    def __init__(self, restaurant_name, cuisine_type, status):
+        """Initialize attributes to describe a Icecreamstands."""
+        super().__init__(restaurant_name, cuisine_type, status)
+        
+        self.flavors = ['apple', 'orange', 'banana']
+
+    def show_flavors(self):
+        """Display flavors lists"""
+
+        print("\nflavors list in menu:")
+        for flavor in self.flavors:
+             print(f"- {flavor.title()}")
+
+restaurant = Restaurant('parsian', 'persian food', 'open')
+icecreamstand = IceCreamStand('parsian', 'icecream', 'close')
+
+print(f"Name attribute : {restaurant.restaurant_name.title()}")
+print(f"Cuisine type : {restaurant.cuisine_type.upper()}\n")
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+print(f"Name attribute : {icecreamstand.restaurant_name.title()}")
+print(f"Cuisine type : {icecreamstand.cuisine_type.upper()}\n")
+icecreamstand.describe_restaurant()
+icecreamstand.open_restaurant()
+icecreamstand.show_flavors()
