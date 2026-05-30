8e2543ae86c632e9aaba9613ec75d13a6acc5c25
shayan <shayanhz.dev@gmail.com>
2026-05-30 10:35:03 -0700

    feat: send POST request with JSON payload and implement raise_for_status

---
+++ Python-Libraries/requests/json_post.py
@@ -0,0 +1,32 @@
+# Practice requests library
+# Send json payload
+# Checking status with raise_for_status
+
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+payload = {
+    "title": "My First Post",
+    "body": "Hello World",
+    "userId": 1
+}
+
+headers = {
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
+try:
+    response = requests.post(
+        url,
+        headers=headers,
+        json=payload,
+        timeout=10
+    )
+    data = response.json()
+    response.raise_for_status()
+    print(data)
+
+except requests.RequestException as error:
+    print(error)
