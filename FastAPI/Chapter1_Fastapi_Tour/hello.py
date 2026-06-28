dda7845af30b0ac01c9bcff694eb25921e8c62e3
shayan <shayanhz.dev@gmail.com>
2026-06-28 07:54:01 -0700

    feat: accept custom header in POST /hi endpoint

--- FastAPI/Chapter1_Fastapi_Tour/hello.py
+++ FastAPI/Chapter1_Fastapi_Tour/hello.py
@@ -2,13 +2,13 @@
 #   My Hello World in FastAPI     #
 ###################################
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Header
 
 app = FastAPI() 
 
 @app.post("/hi") # Decorator
-def great(who:str = Body(embed=True)): # embed=True mean we need json formatted request body
-    return f"Hello? {who}"
+def great(who:str = Header()): 
+    return f"Hello? {who}?"
 
 if __name__ == "__main__" :
     import uvicorn
