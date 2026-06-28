a33c19e6813658dfb4a54a94bfad1309719be2c4
shayan <shayanhz.dev@gmail.com>
2026-06-28 09:07:31 -0700

    feat: add /happy endpoint with explicit HTTP status code

--- FastAPI/Chapter1_Fastapi_Tour/hello.py
+++ FastAPI/Chapter1_Fastapi_Tour/hello.py
@@ -10,6 +10,10 @@ app = FastAPI()
 def get_agent(user_agent:str = Header()): 
     return user_agent
 
+@app.get("/happy", status_code=200)
+def happy():
+    return ":)"
+
 if __name__ == "__main__" :
     import uvicorn
     uvicorn.run("hello:app", reload=True)
