8b1286c06bc95780a324feec5b824e75f3bb1b73
shayan <shayanhz.dev@gmail.com>
2026-06-30 09:13:05 -0700

    feat: add async endpoint example with uvicorn runner

---
+++ FastAPI/Chapter1_Fastapi_Tour/greet_async_uvicorn.py
@@ -0,0 +1,17 @@
+###############################
+# Greet Async Uvicorn Example #
+###############################
+
+from fastapi import FastAPI
+import asyncio
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/hi")
+async def greet():
+    await asyncio.sleep(1)
+    return "Hello? World?"
+
+if __name__ == "__main__":
+    uvicorn.run("greet_async_uvicorn:app", reload=True)
