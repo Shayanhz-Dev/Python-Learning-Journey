61cb13500bf82de4a6517aed092f53f0c11d99c8
shayan <shayanhz.dev@gmail.com>
2026-06-30 09:11:32 -0700

    feat: add async greeting endpoint with delay simulation

---
+++ FastAPI/Chapter1_Fastapi_Tour/greet_async.py
@@ -0,0 +1,13 @@
+############################
+# Greet Async with FastAPI #
+############################
+
+from fastapi import FastAPI
+import asyncio
+
+app = FastAPI()
+
+@app.get("/hi")
+async def greet():
+    await asyncio.sleep(1)
+    return "Hello? World?"
